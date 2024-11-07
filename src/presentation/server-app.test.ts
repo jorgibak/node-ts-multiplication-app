42845fb24c1b1e7445e4122e1e831c68c978e6af
Jorge Espejo Ibacache <jorge.espejo.ibacache@gmail.com>
2024-11-07 12:51:46 -0300

    ServerApp - Pruebas Unitarias

--- src/presentation/server-app.test.ts
+++ src/presentation/server-app.test.ts
@@ -14,6 +14,10 @@ describe('Server App', () => {
         fileName: 'test-filename',
     }
 
+    beforeEach(()=> {
+        jest.clearAllMocks();
+    });
+
     
     test('should create ServerApp instance', () => {
 
@@ -24,31 +28,59 @@ describe('Server App', () => {
 
     test('should run ServerApp with options', () => {
 
-        const logSpy = jest.spyOn(console, 'log');
-        const createTableSpy = jest.spyOn(CreateTable.prototype, 'execute');
-        const saveFileSpy = jest.spyOn(SaveFile.prototype, 'execute');
+        // const logSpy = jest.spyOn(console, 'log');
+        // const createTableSpy = jest.spyOn(CreateTable.prototype, 'execute');
+        // const saveFileSpy = jest.spyOn(SaveFile.prototype, 'execute');
     
 
-        ServerApp.run(options);
+        // ServerApp.run(options);
 
-        expect(logSpy).toHaveBeenCalledTimes(2);
-        expect(logSpy).toHaveBeenCalledWith('Server running...');
-        expect(logSpy).toHaveBeenLastCalledWith('File Created');
+        // expect(logSpy).toHaveBeenCalledTimes(2);
+        // expect(logSpy).toHaveBeenCalledWith('Server running...');
+        // expect(logSpy).toHaveBeenLastCalledWith('File Created');
 
-        expect(createTableSpy).toHaveBeenCalledTimes(1);
-        expect(createTableSpy).toHaveBeenCalledWith({
-            "base": options.base, "limit": options.limit
-        });
+        // expect(createTableSpy).toHaveBeenCalledTimes(1);
+        // expect(createTableSpy).toHaveBeenCalledWith({
+        //     "base": options.base, "limit": options.limit
+        // });
 
-        expect(saveFileSpy).toHaveBeenCalledTimes(1);
-        expect(saveFileSpy).toHaveBeenCalledWith({
-            fileContent: expect.any(String),
-            fileDestination: options.fileDestination,  
-            fileName: options.fileName
-        });
+        // expect(saveFileSpy).toHaveBeenCalledTimes(1);
+        // expect(saveFileSpy).toHaveBeenCalledWith({
+        //     fileContent: expect.any(String),
+        //     fileDestination: options.fileDestination,  
+        //     fileName: options.fileName
+        // });
 
 
         
+    });
+
+    test('should run with custom values mocked', () => {
+
+        const logMock = jest.fn();
+        const logErrorMock = jest.fn();
+        const createMock = jest.fn().mockReturnValue('1 x 2 = 2');
+        const saveFileMock = jest.fn().mockReturnValue(true);
+
+        global.console.log = logMock;
+        global.console.error = logErrorMock;
+        // connect execute method
+        CreateTable.prototype.execute = createMock;
+        SaveFile.prototype.execute = saveFileMock;
+
+        ServerApp.run(options);
+
+        expect(logMock).toHaveBeenCalledWith('Server running...');
+        expect(createMock).toHaveBeenCalledWith({"base": options.base, "limit": options.limit});
+        expect(saveFileMock).toHaveBeenCalledWith({
+            fileContent: '1 x 2 = 2',
+            fileDestination: options.fileDestination,
+            fileName: options.fileName
+        });
+
+        expect(logMock).toHaveBeenCalledWith('File Created');
+        expect(logErrorMock).not.toHaveBeenCalled();
+
     });
     
 })
